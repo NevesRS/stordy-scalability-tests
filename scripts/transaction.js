7a7357d0da01797e1d5a31b70fd3e9db1cfbd337
NevesRS <brunobealnev@gmail.com>
2024-05-15 17:17:49 -0300

    ajeitando o transaction

--- scripts/transaction.js
+++ scripts/transaction.js
@@ -15,89 +15,59 @@ const transactionService = protoDescriptor.stordy.transaction.TransactionService
 const client = new transactionService('localhost:50051', grpc.credentials.createInsecure());
 
 let transactionsQt = 10; //Indique a quantidade de transações a serem inseridas após 499.999
-let counterAdd = 0;
-let counterFind = 0;
-let arrTransactions = [];
 
-// Array para armazenar os tempos de inserção e busca
-let timingData = [];
-
-for (let i = 0; i < (transactionsQt * 2) + 1; i++) {
-    const qtd = (i % 2 === 0) ? 1 : 499999;
-    const transaction = {
-        bpk: "block0",
-        qtd: qtd,
-        transaction: {
-            data: "non" + i,
-            hash: "nostrud nisi" + i,
-            identification: "Lorem consectetur magna Excepteur et" + i,
-            index: 3489093785 + i,
-            nonce: 948774202 + i,
-            previousHash: "in" + i,
-            signature: "nisi" + i,
-            timestamp: "41380415" + i
-        }
-    };
-    arrTransactions.push(transaction);
+async function processTransactions() {
+    for (let i = 0; i < transactionsQt; i++) {
+        const qtd = (i % 2 === 0) ? 1 : 499999;
+        const transaction = {
+            bpk: "block0",
+            qtd: qtd,
+            transaction: {
+                data: "non" + i,
+                hash: "nostrud nisi" + i,
+                identification: "Lorem consectetur magna Excepteur et" + i,
+                index: 3489093785 + i,
+                nonce: 948774202 + i,
+                previousHash: "in" + i,
+                signature: "nisi" + i,
+                timestamp: "41380415" + i
+            }
+        };
+        await sendTransaction(transaction); // Aguarda a conclusão da transação atual antes de prosseguir
+    }
 }
-
-// console.log("Time taken to insert 1 transaction after 499.999 inserted into a block")
 
 async function sendTransaction(transaction) {
     return new Promise((resolve, reject) => {
-        const startTime = now();
+        const start = now();
         client.AddTransaction(transaction, (error, response) => {
             if (error) {
                 reject(error);
             } else {
-                if (counterAdd % 2 == 0) {
-                    const endTime = now();
-                    const timeElapsedInMs = endTime - startTime;
-                    timingData.push({ AddTransaction: timeElapsedInMs, FindLastTransaction: null });
-                }
-                counterAdd++;
+                if (transaction.qtd == 1) {
+                    const end = now();
+                    const elapsedTime = end - start;
+                    console.log('Tempo para inserir ' + transaction.qtd + " transações: " + elapsedTime.toFixed(0) + 'ms');
 
-                const findLastTransactionRequest = { bpk: transaction.bpk };
-                const findStartTime = now();
-                client.FindLastTransaction(findLastTransactionRequest, (findError, findResponse) => {
-                    if (findError) {
-                        reject(findError);
-                    } else {
-                        if (counterFind % 2 == 0) {
-                            const findEndTime = now();
-                            const findTimeElapsedInMs = findEndTime - findStartTime;
-                            timingData[timingData.length - 1].FindLastTransaction = findTimeElapsedInMs;
+                    const findLastTransactionRequest = { bpk: transaction.bpk };
+                    const startFind = now();
+                    client.FindLastTransaction(findLastTransactionRequest, (findError, findResponse) => {
+                        if (findError) {
+                            reject(findError);
+                        } else {
+                            const endFind = now();
+                            const elapsedTimeFind = endFind - startFind;
+                            console.log('Tempo para pesquisar a transação: ' + elapsedTimeFind.toFixed(0) + 'ms');
                         }
-                        counterFind++;
-                        resolve(response);
-                    }
-                });
+                    });
+                }
+                resolve(response);
             }
         });
     });
 }
 
-async function sendAllTransactions() {
-    for (const transaction of arrTransactions) {
-        await sendTransaction(transaction);
-    }
-}
+processTransactions().catch(error => {
+    console.error('Erro ao processar transações:', error);
+});
 
-sendAllTransactions()
-    .then(() => {
-        console.log("All transactions have been sent!");
-        // Preparar os dados para escrita no arquivo CSV
-        const csvData = timingData.map(entry => ({
-            AddTransaction: entry.AddTransaction.toFixed(0) + 'ms',
-            FindLastTransaction: entry.FindLastTransaction ? entry.FindLastTransaction.toFixed(0) + 'ms' : ''
-        }));
-
-        // Converter os dados para formato CSV
-        const csv = parse(csvData, { fields: ['AddTransaction', 'FindLastTransaction'] });
-
-        // Escrever os dados no arquivo
-        fs.writeFileSync('formatted_data_transaction.csv', csv, { flag: 'a' });
-        fs.appendFileSync('formatted_data_transaction.csv', '\n');
-        console.log("Formatted timing data written to formatted_data_transaction");
-    })
-    .catch(error => console.error("An error has occurred:", error));
